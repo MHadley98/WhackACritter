4f0979b984812b99bdbb1d4e8069aed2253774b5
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2018-09-21 16:50:49 +0100

    Added critter rotation

--- Assets/Scripts/Critter.cs
+++ Assets/Scripts/Critter.cs
@@ -12,16 +12,29 @@ public class Critter : MonoBehaviour {
 
     public int pointValue=1;//How much is this critter worth?
 
+    private Vector2 direction;//what direction critter travels in
+
 	// Use this for initialization
 	void Start () {
         transform.position = new Vector3(Random.Range(lowerRange.x,upperRange.x),
             Random.Range(lowerRange.y, upperRange.y), 
             0);
-	
-	}
+
+        //pick a random direction for critter
+        direction.x = Random.Range(-1.0f,1.0f);
+        direction.y = Random.Range(-1.0f,1.0f);
+        direction = direction.normalized;
+
+        //rotate critter in  this direction
+        transform.rotation = Quaternion.FromToRotation(Vector3.up,direction);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        transform.Translate(Vector2.up * Time.deltaTime);
+
         if(timer.IsTimerRunning()==false)
         {
             Destroy(gameObject);
